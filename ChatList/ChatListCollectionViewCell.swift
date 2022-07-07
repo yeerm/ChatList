e48ccc9324a87a0d39b00243f258d0065ed5d35c
yeerm <yeerm6931@gmail.com>
2022-07-07 22:17:05 +0900

    first commit

---
+++ ChatList/ChatListCollectionViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  ChatListCollectionViewCell.swift
+//  ChatList
+//
+//  Created by MacBook Pro on 2022/07/04.
+//
+
+import UIKit
+
+class ChatListCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var chatLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    func configure(_ chat: Chat) {
+        thumbnail.image = UIImage(named: chat.name)
+        nameLabel.text = chat.name
+        chatLabel.text = chat.chat
+        dateLabel.text = formattedDateString(dateString:chat.date)
+    }
+    
+    func formattedDateString(dateString: String) -> String {
+        //String -> date -> String
+        //2022-04-01 > 04/01
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        
+        //String -> date
+       if let date = formatter.date(from: dateString){
+           formatter.dateFormat = "M/d"
+           return formatter.string(from: date)
+       }else {
+           return ""
+       }
+    }
+}
---
+++ ChatList/ChatListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ChatListViewController.swift
+//  ChatList
+//
+//  Created by MacBook Pro on 2022/07/04.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let chatList : [Chat] = Chat.list
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Data, Presentation, Layout
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+}
+extension ChatListViewController:
+    UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return chatList.count
+    }
+    
+    //Presentation
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChatListCollectionViewCell", for: indexPath) as? ChatListCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let chat = chatList[indexPath.item]
+        cell.configure(chat)
+        return cell
+    }
+}
+
+extension ChatListViewController:
+    UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 100)
+    }
+}
